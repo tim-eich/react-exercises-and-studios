5f9bdfaab24f6b017d7480028232db119d4b8071
Tim-E <caffeinated.ink@gmail.com>
2024-03-14 18:05:15 -0400

    In-progress commit

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Reading List";
+   let book1 = "https://www.pluggedin.com/wp-content/uploads/2020/01/hitchhikers-guide-to-galaxy-cover-image-1024x1536.jpeg";
+   let book2 = "https://i.harperapps.com/hcanz/covers/9780732291501/y648.jpg";
+   let book3 = "https://imgv2-2-f.scribdassets.com/img/word_document/163599595/original/f284089a34/1600024134?v=1";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Hitchhiker's Guide to the Galaxy" />
+         <img src={book2} alt="John Dies at the End" />
+         <img src={book3} alt="Cryptonomicon" />
       </div>      
    );
 }
