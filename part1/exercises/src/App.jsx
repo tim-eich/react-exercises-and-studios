bea87795b73a658e797d6737c40ae2a4a1ebca17
Tim-E <caffeinated.ink@gmail.com>
2024-03-17 23:53:04 -0400

    Resized images, temp CSS fix

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
     <ChoresList />
-      <div className="similarComponents">
+      <div className='similarComponents'>
         <MovieList />
         <BookList />
         <HobbyLinks />
