5b090a9d071bc0dbb2daf4edaead502d44c001ac
Tim-E <caffeinated.ink@gmail.com>
2024-03-20 09:28:42 -0400

    parts 1 & 2 complete

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import './styles.css';
 
 function Button() {
-
+   const onLearnMore = () => window.alert("Approximately 100,000,000 sharks are removed from the ocean annually.");
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
