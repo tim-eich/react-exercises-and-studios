c8a2ca2e72a574e8708d3a8b6adf02a9b2f15fdf
Tim-E <caffeinated.ink@gmail.com>
2024-03-20 10:33:50 -0400

    part2 exercises complete and rendering correctly

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,27 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
 
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+function Profile() {
+   const listItem = oceans.map( ocean => 
+      <div className={`${ocean.fishCheck ? "isAFish" : "profile"}`} key ={ ocean.id }>
+         <img className = 'img' src ={ ocean.image } alt ={ ocean.name }/>
+         <h1>{ ocean.name }</h1>
+         <h3>Some Facts:</h3>
+         <ol>
+            <li>{ ocean.fact1 }</li>
+            <li>{ ocean.fact2 }</li>
+            <li>{ ocean.fact3 }</li>
+         </ol>
          <Button />
-      </>  
+      </div>
+   );
+
+   return(
+         <ul>
+            { listItem }
+         </ul>
    );
 }
 
